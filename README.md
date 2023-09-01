694ed3dbdc683619066d0c4ad92af8a1bb65207e
Maya Bachvarova <may13@abv.bg>
2023-09-01 13:54:36 +0300

    Custom countdown

---
+++ README.md
@@ -0,0 +1,2 @@
+Custom countdown project.
+Start here.
---
+++ script.js
@@ -0,0 +1,4 @@
+const inputContainer = document.querySelector('.input-container');
+const countdownForm = document.querySelector('.countdownForm');
+const dateEl = document.querySelector('.date-picker');
+
